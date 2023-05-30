8298b4d96a6919412e20d6a9a6e20a146792df0b
SelinaKiteki1 <selinakiteki@gmail.com>
2023-05-30 03:12:17 -0700

    get_size.c

---
+++ get_size.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* get_size - Calculates the size to give the argument in the function
+* @format:string to print arguments in the function
+* @i: List of all the  arguments to be printed in the function.
+* Return:(Precision).
+*/
+int get_size(const char *format, int *i)
+{
+int curr_i = *i + 1;
+int size = 0;
+
+if (format[curr_i] == 'l')
+size = S_LONG;
+else if (format[curr_i] == 'h')
+size = S_SHORT;
+
+if (size == 0)
+*i = curr_i - 1;
+else
+*i = curr_i;
+
+return (size);
+}
